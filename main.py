75834620a3c077c4e10369d1a13c270fae7c54df
YiChen9169 <142741416+YiChen9169@users.noreply.github.com>
2024-01-22 14:42:18 +0800

    simplify upload sound

--- main.py
+++ main.py
@@ -60,13 +60,17 @@ power_imgs['snowflower'] = load_image("snowflower.png")
 power_imgs['snowflower'] = pygame.transform.scale(power_imgs['snowflower'], (50, 50))
 
 # 載入音樂、音效
-shoot_sound = pygame.mixer.Sound(os.path.join("sound", "shoot.wav"))
-gun_sound = pygame.mixer.Sound(os.path.join("sound", "pow1.wav"))
-shield_sound = pygame.mixer.Sound(os.path.join("sound", "pow0.wav"))
-die_sound = pygame.mixer.Sound(os.path.join("sound", "rumble.ogg"))
+def load_sound(file_name):
+    return pygame.mixer.Sound(os.path.join("sound", file_name))
+
+shoot_sound = load_sound("shoot.wav")
+
+gun_sound = load_sound("pow1.wav")
+shield_sound = load_sound("pow0.wav")
+die_sound = load_sound("rumble.ogg")
 expl_sounds = [
-    pygame.mixer.Sound(os.path.join("sound", "expl0.wav")),
-    pygame.mixer.Sound(os.path.join("sound", "expl1.wav"))
+    load_sound("expl0.wav"),
+    load_sound("expl1.wav")
 ]
 pygame.mixer.music.load(os.path.join("sound", "background.ogg"))
 pygame.mixer.music.set_volume(0.4)
